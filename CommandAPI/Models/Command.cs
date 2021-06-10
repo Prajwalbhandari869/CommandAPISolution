47eb9195c8bcdc400f0bff596fcbd8cd83461ea5
unknown <prajwalbhandari869@gmail.com>
2021-06-10 12:22:07 +0545

    added command model

---
+++ CommandAPI/Models/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
+
