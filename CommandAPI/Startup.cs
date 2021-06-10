a59f710819b0cdc236399941796f4c0af1a77c73
Prajwal Barista <prajwal.bhandari869@hotmail.com>
2021-06-10 22:01:32 +0545

    chap7fin

--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -2,6 +2,8 @@ using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,12 +15,21 @@ namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();// dependency injection 
+            services.AddDbContext<CommandContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DbContextConnectionString")));
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();// dependency injection
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
