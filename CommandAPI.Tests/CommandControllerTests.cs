70f09933c56aa8a6b28260c366219d0c1febbd08
Prajwal Barista <prajwal.bhandari869@hotmail.com>
2021-06-14 21:22:06 +0545

    asdjflkja

---
+++ CommandAPI.Tests/CommandControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Xunit;
+using CommandAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using Moq;
+using System.Collections.Generic;
+using CommandAPI.Models;
+using CommandAPI.Profiles;
+using AutoMapper;
+using CommandAPI.Dtos;
+
+namespace CommandAPI.Tests
+{
+    public class CommandControllerTests : IDisposable
+    {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+        public CommandControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+        public void Dispose()
+        {
+            mockRepo = null;
+            realProfile = null;
+            configuration = null;
+            mapper = null;
+        }
+
+        [Fact]
+        public void GetCommandItems_Returns2000k_WhenDbIsEmpty()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetAllCommands()).Returns(GetCommands(0));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Arrange
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+        [Fact]
+        public void GetAllCommands_Returns2000k_WhenDbHasOneResources()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public void GetAllCommands_ReturnsCorrectType_WhenDbHasOneResources()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object,mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            Assert.IsType<ActionResult<IEnumerable<CommandReadDto>>>(result);
+        }
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How To Generate a Migration",
+                    CommandLine = "dotnet ef mmigration add <name of migration>",
+                    Platform = " .net core EF"
+                });
+            }
+            return commands;
+        }
+    }
+}
---
+++ CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using CommandAPI.Models;
+namespace CommandAPI.Tests
+{
+    public class CommandTests :IDisposable
+    {
+        Command testCommand;
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+           
+            //Act
+            testCommand.HowTo = "Execute unit test";
+            //Assert
+            Assert.Equal("Execute unit test", testCommand.HowTo);
+        }
+        [Fact]
+        public void CanChangePlatforn()
+        {
+            //Arrange
+
+            //Acts
+            testCommand.Platform = "Prajwal";
+            //Assert
+            Assert.Equal("Prajwal", testCommand.Platform);
+        }
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Acts
+            testCommand.CommandLine = "Bhandari";
+            Assert.Equal("Bhandari",testCommand.CommandLine);
+        }
+
+    }
+}
